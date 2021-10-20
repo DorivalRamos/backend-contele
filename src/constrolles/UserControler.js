396f2c1a6cf68d59401f93613b1873880be883d8
DorivalRamos <dorivalmillan2@hotmail.com>
2021-10-20 18:43:45 -0300

    Finalizando o CRUD dos Usuarios

--- src/constrolles/UserControler.js
+++ src/constrolles/UserControler.js
@@ -5,4 +5,44 @@ module.exports = {
     const results = await knex("users");
     return res.json(results);
   },
+
+  async create(req, res, next) {
+    try {
+      const { username, userPassword, userEmail } = req.body;
+      await knex("users").insert({
+        username,
+        userPassword,
+        userEmail,
+      });
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const { userName, userPassword, userEmail } = req.body;
+      const { id } = req.params;
+      await knex("users")
+        .update({ userName, userEmail, userPassword })
+        .where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      await knex("users").where({ id }).del();
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const { application_name } = require("pg/lib/defaults");
 const routes = require("./routes");
 
 const app = express();
+app.use(express.json());
 app.use(routes);
+
+//notFound
+app.use((req, res, next) => {
+  const error = new Error("NotFound");
+  error.status = 404;
+  next(error);
+});
+//catch all
+app.use((error, req, res, next) => {
+  //error 500 internal server error
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 app.listen(3333, () => console.log("server is running"));
