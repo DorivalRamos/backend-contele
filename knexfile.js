2d3eb98fc08efb2a430fef6ebfddc8bba210c3c0
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-23 23:05:21 -0300

    Adjustments: Making adjustments before review for delivery
    Alguns ajustes foram feitos para que se possa fazer algumas revisões para a entrega do projeto

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,23 @@
 // Update with your config settings.
 
-module.exports = {
-  development: {
-    client: "pg",
-    version: "7.2",
-    connection: {
-      host: "localhost",
-      database: "knex_test",
-      user: "pguser",
-      password: "pgpassword",
-      port: 5432,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: `${__dirname}/src/database/migrations`,
-    },
-    seeds: {
-      directory: `${__dirname}/src/database/seeds`,
-    },
-  },
-};
+module.exports = ({
+  development :{
+      client: 'pg',
+      version: '7.2',
+      connection: {
+        host : 'localhost',
+        port : 5432,
+        user : 'pguser',
+        password : 'pgpassword',
+        database : 'knex_test'
+      },
+      migrations: {
+        tableName: "knex_migrations",
+        directory: `${__dirname}/src/database/migrations`,
+      },
+      seeds: {
+        directory: `${__dirname}/src/database/seeds`,
+      },}
+  
+});
+
