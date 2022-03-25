77511993877b567bfa0a1f9e4134d166fa48ed0e
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-24 23:24:33 -0300

    Fix: Route Post RegisterSchema

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const routes = express.Router();
 
-const UserController = require("./constrolles/UserControler");
+const UserController = require("./middlewares/UserControler");
+const registerSchema = require("./middlewares/register-schema");
 
 routes.get("api-docs");
 routes.get("/api/v1/hellow", UserController.hello)
 routes.get("/api/v1/users", UserController.getAllUsers)
-routes.post("/api/v1/users", UserController.create);
+routes.post("/api/v1/users",registerSchema, UserController.create);
 routes.delete("/api/v1/users", UserController.deleteAll);
 routes.get("/api/v1/usersById/:id", UserController.getUsersById);
 routes.put("/api/v1/users/:id", UserController.update);
