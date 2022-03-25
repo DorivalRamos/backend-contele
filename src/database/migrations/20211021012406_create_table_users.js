cb998d49787f510d7aa56798274d74ecd1432bd8
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-24 23:59:42 -0300

    bug: DockerCompose

--- src/database/migrations/20211021012406_create_table_users.js
+++ src/database/migrations/20211021012406_create_table_users.js
@@ -2,7 +2,7 @@
 exports.up = (knex) =>
   knex.schema.createTable("users", (table) => {
     table.increments("id");
-    table.text("username").unique().notNullable();
+    table.text("userName").notNullable();
     table.text("userEmail").unique().notNullable();
     table.text("userPassword").unique().notNullable();
     table.timestamp("create_at").defaultTo(knex.fn.now());
