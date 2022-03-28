4b70989cbd5444745e59943dd1daa9c46ee9406b
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-28 16:22:13 -0300

    fix: routes

--- routes.js
+++ routes.js
@@ -6,12 +6,12 @@ const UserController = require("./src/UserControler");
 const registerSchema = require("./src/register-schema");
 
 routes.get("api-docs");
-routes.get("/api/v1/hellow", UserController.hello)
-routes.get("/api/v1/users", UserController.getAllUsers)
-routes.post("/api/v1/users",registerSchema, UserController.create);
+routes.get("/api/v1/hellow", UserController.hello);
+routes.get("/api/v1/users", UserController.getAllUsers);
+routes.post("/api/v1/users", registerSchema, UserController.create);
 routes.delete("/api/v1/users", UserController.deleteAll);
 routes.get("/api/v1/usersById/:id", UserController.getUsersById);
-routes.put("/api/v1/users/:id", UserController.update);
+routes.patch("/api/v1/users/:id", registerSchema, UserController.update);
 routes.delete("/api/v1/users/:id", UserController.delete);
 
 module.exports = routes;
