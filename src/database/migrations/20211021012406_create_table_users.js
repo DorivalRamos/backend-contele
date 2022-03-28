712b9916d6340fb9a931b9f721b92101b07351b9
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-28 16:19:56 -0300

    Update 20211021012406_create_table_users.js

--- src/database/migrations/20211021012406_create_table_users.js
+++ src/database/migrations/20211021012406_create_table_users.js
@@ -1,10 +1,9 @@
-
 exports.up = (knex) =>
   knex.schema.createTable("users", (table) => {
     table.increments("id");
     table.text("userName").notNullable();
     table.text("userEmail").unique().notNullable();
-    table.text("userPassword").unique().notNullable();
+    table.text("userPassword").notNullable();
     table.timestamp("create_at").defaultTo(knex.fn.now());
     table.timestamp("update_at").defaultTo(knex.fn.now());
   });
