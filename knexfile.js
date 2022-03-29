680786a3f2b9a2378813b07bb1a7a54d2461c9cc
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-29 19:34:00 -0300

    fix : knexFile with process.env

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,22 @@
 // Update with your config settings.
-
-module.exports = ({
-  development :{
-      client: 'pg',
-      version: '7.2',
-      connection: {
-        host : 'pgsql',
-        port : 5432,
-        user : 'pguser',
-        password : 'pgpassword',
-        database : 'knex_test'
-      },
-      migrations: {
-        tableName: "knex_migrations",
-        directory: `${__dirname}/src/database/migrations`,
-      },
-      seeds: {
-        directory: `${__dirname}/src/database/seeds`,
-      },}
-  
-});
-
+require("dotenv").config();
+module.exports = {
+  development: {
+    client: "pg",
+    version: "7.2",
+    connection: {
+      host: process.env.HOST_ENV,
+      port: process.env.DBPORT_ENV,
+      user: process.env.USER_ENV,
+      password: process.env.PASSWORD_ENV,
+      database: process.env.DATABASE_ENV,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
+};
