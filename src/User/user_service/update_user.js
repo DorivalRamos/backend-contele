42dc501d85eb024d7a998b5f919c68695c19b95f
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-29 21:38:25 -0300

    fix : refactoring update_user

--- src/User/user_service/update_user.js
+++ src/User/user_service/update_user.js
@@ -3,18 +3,16 @@ const find_user_by_id = require("./find_user_by_id");
 const user_repository = require("../../user_repository");
 
 const update_user = async (req, res, next) => {
-  const user = req.body;
-  const results = await find_user_by_id.find_user_by_id(req, res);
-  try {
-    const updatedUser = await user_repository.UpdateUserRepository(
-      results,
-      user
-    );
+  const id = req.params;
 
-    return res
-      .status(201)
-      .json({ success: "Usuario Atualizado com sucesso", updatedUser });
+  try {
+    const is_exist = await find_user_by_id.find_user_by_id2(id.id);
+    const updatedUser = await user_repository.update_user_repository(id, req);
+    return res.status(201).json({ success: "Usuario Atualizado com sucesso" });
   } catch (error) {
+    if (error.code.toString() === "23505") {
+      return res.status(400).json("Endereço de e-mail já está em uso!");
+    }
     next(error);
   }
 };
